a1264098a43381f6f46fb9c91ee7e32b8f03870e
Noa Ette <noaemien@gmail.com>
2025-05-15 21:34:18 +0200

    feat: added optional path argument to fzf_tmux and made jump shorcut use fzf_tmux

--- shared/scripts/scripts/generateShortcuts.py
+++ shared/scripts/scripts/generateShortcuts.py
@@ -5,7 +5,7 @@ with open('/Users/emien/.config/bash/nav_folders.json', 'r') as s:
 
 buf = "#!/bin/bash\n"
 for key,path in data.items():
-  buf += "alias " + key + "='cd " + path +"'\n"
+  buf += "alias " + key + "='fzf_tmux " + path +"'\n"
 
 with open('/Users/emien/.config/bash/shortcuts.sh', 'w+') as s:
   print(buf, file=s)
