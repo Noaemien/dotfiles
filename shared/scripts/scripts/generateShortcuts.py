7ec92a5ccb34d075262e3390bb8542698c373fbd
Noa Ette <noaemien@gmail.com>
2025-05-27 19:47:51 +0200

    fix: changed shortcuts to use , prefix

--- shared/scripts/scripts/generateShortcuts.py
+++ shared/scripts/scripts/generateShortcuts.py
@@ -3,22 +3,26 @@ import os
 
 
 # FOLDERS
+# print(os.environ["HOME"])
 with open(os.environ.get("HOME", "/Users/emien") + '/.config/bash/nav_folders.json', 'r') as s:
   data = json.load(s)
 
 bash_buf = "#!/bin/bash\n"
 ranger_buf = ""
+vim_buf = ""
 for key,path in data.items():
-  bash_buf += "alias g" + key + "='fzf_tmux " + path +"'\n"
-  ranger_buf += "map g" + key + " cd " + path + "\n"
+  bash_buf += "alias ,g" + key + "='fzf_tmux " + path +"'\n"
+  ranger_buf += "map ,g" + key + " cd " + path + "\n"
+  vim_buf += "vim.keymap.set({'n', 'v'}, '<leader>g" + key + "', ':e ' .. vim.fn.fnameescape('" + path +"') .. '<cr>')\n"
 
 # CONFIGS
 with open('/Users/emien/.config/bash/nav_config.json', 'r') as s:
   data = json.load(s)
 
 for key,path in data.items():
-  bash_buf += "alias c" + key + "='nvim " + path +"'\n"
-  ranger_buf += "map c" + key + " chain cd " + os.path.dirname(path) + "; search " + os.path.basename(path) +"; open_with nvim " + "\n"
+  bash_buf += "alias ,c" + key + "='nvim " + path +"'\n"
+  ranger_buf += "map ,c" + key + " chain cd " + os.path.dirname(path) + "; search " + os.path.basename(path) +"; open_with nvim " + "\n"
+  vim_buf += "vim.keymap.set({'n', 'v'}, '<leader>c" + key + "', ':e ' .. vim.fn.fnameescape('" + path +"') .. '<cr>')\n"
 
 # SAVE TO FILES 
 
@@ -27,3 +31,6 @@ with open('/Users/emien/.config/bash/shortcuts.sh', 'w+') as s:
 
 with open('/Users/emien/.config/ranger/shortcuts.conf', 'w+') as s:
   print(ranger_buf, file=s)
+
+with open('/Users/emien/.config/nvim/lua/shortcuts.lua', 'w+') as s:
+  print(vim_buf, file=s)
