44114b430353704cbcdfbdf63eaf636a960c609e
Noa Ette <noaemien@gmail.com>
2025-07-05 23:50:05 +0200

    feat: treesitter config

--- shared/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ shared/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,46 +4,100 @@ return {
     require("nvim-treesitter.install").update({ with_sync = true })
   end,
   dependencies = {
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      opts = {
-        custom_calculation = function(_, language_tree)
-          if vim.bo.filetype == "blade" and language_tree._lang ~= "javascript" and language_tree._lang ~= "php" then
-            return "{{-- %s --}}"
-          end
-        end,
-      },
-    },
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
-  opts = {
-    ensure_installed = "all",
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-    -- Needed because treesitter highlight turns off autoindent for php files
-    indent = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    ---@class ParserInfo[]
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = {
-          "src/parser.c",
-          -- 'src/scanner.cc',
-        },
-        branch = "main",
-        generate_requires_npm = true,
-        requires_generate_from_grammar = true,
-      },
-      filetype = "blade",
-    }
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
-    require("nvim-treesitter.configs").setup(opts)
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+
+      indent = { enable = true },
+      highlight = {
+        enable = true,
+        -- disable for large files
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024   -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+
+        additional_vim_regex_highlighting = false, -- Check if true is needed
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = ",i",
+          node_incremental = "+",
+          scope_incremental = "grc",
+          node_decremental = "_",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+
+          -- Automatically jump forward to textobjects, similar to targets.vim
+          lookahead = true,
+
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = { query = "@function.outer", desc = "around a function" },
+            ["if"] = { query = "@function.inner", desc = "inner part of a function" },
+            ["ac"] = { query = "@class.outer", desc = "around a class" },
+            ["ic"] = { query = "@class.inner", desc = "inner part of a class" },
+            ["ai"] = { query = "@conditional.outer", desc = "around an if statement" },
+            ["ii"] = { query = "@conditional.inner", desc = "inner part of an if statement" },
+            ["al"] = { query = "@loop.outer", desc = "around a loop" },
+            ["il"] = { query = "@loop.inner", desc = "inner part of a loop" },
+            ["ap"] = { query = "@parameter.outer", desc = "around parameter" },
+            ["ip"] = { query = "@parameter.inner", desc = "inside a parameter" },
+          },
+          -- You can choose the select mode (default is charwise 'v')
+          selection_modes = {
+            ['@parameter.outer'] = 'v',                   -- charwise
+            ['@parameter.inner'] = 'v',                   -- charwise
+            ["@function.outer"] = "V",                    -- linewise
+            ["@conditional.outer"] = "V",                 -- linewise
+            ["@loop.outer"] = "V",                        -- linewise
+            ['@class.outer'] = '<c-v>',                   -- blockwise
+          },
+        },
+      move = {
+        enable = true,
+        set_jumps = true,                 -- whether to set jumps in the jumplist
+        goto_previous_start = {
+          ["gpf"] = { query = "@function.outer", desc = "Previous function" },
+          ["gpl"] = { query = "@loop.outer", desc = "Previous loop" },
+          ["gpi"] = { query = "@conditional.outer", desc = "Previous if" },
+          ["gpc"] = { query = "@class.outer", desc = "Previous class" },
+          ["gpp"] = { query = "@parameter.inner", desc = "Previous parameter" },
+        },
+        goto_next_start = {
+          ["gnf"] = { query = "@function.outer", desc = "Next function" },
+          ["gnl"] = { query = "@loop.outer", desc = "Previous loop" },
+          ["gni"] = { query = "@conditional.outer", desc = "Previous if" },
+          ["gnc"] = { query = "@class.outer", desc = "Next class" },
+          ["gnp"] = { query = "@parameter.inner", desc = "Next parameter" },
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>u"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>U"] = "@parameter.inner",
+        },
+      },
+      },
+    })
   end,
 }
